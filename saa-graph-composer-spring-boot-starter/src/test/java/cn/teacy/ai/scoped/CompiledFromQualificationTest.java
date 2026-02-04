ca89c62edb7d274cc847e5897727063aa6d4abfd
chrisis <chrisis58@outlook.com>
2026-02-04 16:39:51 +0800

    fix: set web environment to NONE

--- saa-graph-composer-spring-boot-starter/src/test/java/cn/teacy/ai/scoped/CompiledFromQualificationTest.java
+++ saa-graph-composer-spring-boot-starter/src/test/java/cn/teacy/ai/scoped/CompiledFromQualificationTest.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
         classes = {
                 CompiledFromQualificationTest.TestConfig.class,
                 GreetingGraphComposer.class,
